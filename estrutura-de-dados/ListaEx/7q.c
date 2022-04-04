a80d981defe4393cf5bb2d7645e56e32d1ca9d97
Nunes <vnunesaj@gmail.com>
2022-04-04 10:55:38 -0300

    exercicios C

--- estrutura-de-dados/ListaEx/7q.c
+++ estrutura-de-dados/ListaEx/7q.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    float fn[100], sn[100], av;
-    int i, al;
+    float fn[20], sn[20], av[20], tav[0];
+    int i;
+    fflush(stdout);
 
-    printf("Quantidade de alunos na turma: ");
-    scanf("%d", &al);
-    
-    for(i = 0; i < al; i++){
+    // saber média do aluno
+    for(i = 0; i < 3; i++){
         printf("Digite a sua primeira nota: ");
         scanf("%f", &fn[i]);
 
         printf("Digite a sua segunda nota: ");
         scanf("%f", &sn[i]);
 
-        av = (fn[i] * 2 + sn[i] * 3) / 5;
-        printf("Sua 1ª nota é %1.f, sua 2ª é %1.f e portanto sua média é %1.f\n", fn[i], sn[i], av);
+        tav[0] = tav[0] + av[i];
+
+        av[i] = (fn[i] * 2 + sn[i] * 3) / 5;
+        printf("Sua primeira nota eh %.1f, sua segunda eh %.1f e portanto sua media eh %.1f\n", fn[i], sn[i], av[i]);
+        printf("%.1f\n", tav);
     }
-    
-    system("clear");
-    printf("Sua 1ª nota é %1.f, sua 2ª é %1.f e portanto sua média é %1.f\n", fn, sn, av);
+
+    // definir a média da turma
+    for(i = 0; i < 3; i++){
+        tav[0] = tav[0] / 3;
+        if (av[i] > tav[0]){
+            printf("O aluno %d teve resultados acima da media\n", i);
+        }else{
+            printf("O aluno %d teve resultados abaixo da media\n", i);
+        }
+    }
 
     return 0;
 }
